f6108074b77488575982466b30c0f4733fe8d5b3
Kuldeep12e <kuldeep0105yadav@gmail.com>
2025-09-20 13:55:09 +0530

    --amend

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,7 +7,7 @@ describe("sample test endpoint", () => {
   test("should return response on ping", async () => {
     const response = await request.get("/ping");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ msg: "hello" });
+    expect(response.body).toEqual({ msg: "ok" });
   });
 
   afterAll((done) => {
