ba327ee969a5fcbbc41fade0bc92a3df293a0903
Kuldeep12e <kuldeep0105yadav@gmail.com>
2025-09-20 14:14:06 +0530

    added a new test

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,9 +7,10 @@ describe("sample test endpoint", () => {
   test("should return response on ping", async () => {
     const response = await request.get("/ping");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ msg: "ok" });
+    expect(response.body).toEqual({ msg: "ok this " });
   });
-
+   
+  
   afterAll((done) => {
     server.close(done);  // closes the server after tests
   });
